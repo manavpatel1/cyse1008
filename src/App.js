545c2e9e99f929a771324a0fb8e96803b17a9b7c
manavpatel1 <159789166+manavpatel1@users.noreply.github.com>
2024-02-12 17:56:47 +0000

    Added name to App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Manav here Welcomes you. 
         </a>
       </header>
     </div>
